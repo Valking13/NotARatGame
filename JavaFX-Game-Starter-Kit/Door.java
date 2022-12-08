d74aa40bc4d1b133b570a519033e266d4e779fe8
Valking13 <66075461+Valking13@users.noreply.github.com>
2022-12-08 22:59:35 +0000

    everything

---
+++ JavaFX-Game-Starter-Kit/Door.java
@@ -0,0 +1,16 @@
+
+
+import java.util.ArrayList;
+
+public class Door extends Item{
+
+    private ArrayList<Integer> location = new ArrayList<Integer>(); // Create an ArrayList object
+
+    public Door(ArrayList<Integer> location) {
+        this.location = location;
+    }
+
+    public ArrayList<Integer> getLocation() {
+        return location;
+    }
+}
---
+++ JavaFX-Game-Starter-Kit/FlyingAssasin.java
@@ -0,0 +1,11 @@
+
+
+
+public class FlyingAssasin extends Entity {
+    private int[] locationCoord = {0,0};
+    private int velocityX;
+    private int velocityY;
+    private int[] startingLocation;
+    private String directionFacing;
+
+}
