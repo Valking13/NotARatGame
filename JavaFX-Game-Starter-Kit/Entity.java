db987e89a05090b379d8995e9c9524db1451a394
Valking13 <66075461+Valking13@users.noreply.github.com>
2022-12-08 23:10:11 +0000

    added everything no class paths
    missing class paths

---
+++ JavaFX-Game-Starter-Kit/Entity.java
@@ -0,0 +1,11 @@
+
+
+public abstract class Entity {
+    protected static int boardWidth = 0;
+    protected static int boardHeight = 0;
+
+
+
+
+}
+
---
+++ JavaFX-Game-Starter-Kit/Thieves.java
@@ -0,0 +1,5 @@
+
+
+public abstract class Thieves extends Entity {
+
+}
